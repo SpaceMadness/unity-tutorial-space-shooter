b210a68f7e89568547373984b14d072e0f92ef94
Alex Lementuev <a.lementuev@gmail.com>
2015-07-26 16:08:03 -0700

    Added visual feedback for 'godMode'

--- Assets/Scripts/Cvars.cs
+++ Assets/Scripts/Cvars.cs
@@ -8,4 +8,23 @@ using LunarPlugin;
 static class Cvars
 {
 	public static readonly CVar godMode = new CVar("godMode", false);
+
+	static Cvars()
+	{
+		godMode.AddDelegate(delegate(CVar cvar) {
+
+			if (Application.isPlaying)
+			{
+				GameObject player = GameObject.FindGameObjectWithTag("Player");
+				Material[] materials = player.GetComponent<MeshRenderer>().materials;
+				
+				Color color = cvar.BoolValue ? Color.red : Color.white;
+				foreach (Material m in materials)
+				{
+					m.color = color;
+				}
+			}
+
+		});
+	}
 }
